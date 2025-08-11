7332f2fdd9a031d26ceda67b1b1a51a9bb6d37bf
SaraToth <saratoth101@gmail.com>
2025-08-11 18:22:11 +0900

    Feat: Config passport-jwt

---
+++ config/passport.js
@@ -0,0 +1,30 @@
+const passport = require("passport");
+const jwtStrategy = require("passport-jwt").Strategy;
+const extractJWT = require("passport-jwt").ExtractJwt;
+const prisma = require("../prisma/client");
+
+const options = {
+    jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+};
+
+passport.use(
+    new jwtStrategy(options, async (jwt_payload, done) => {
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: jwt_payload.sub
+                }
+            });
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        } catch (err) {
+            return done(error, false);
+        }
+    })
+);
+
+module.exports = passport;
