088ff6fb901cfe8d8b43e474a73cfa3b4b13ad13
SaraToth <saratoth101@gmail.com>
2025-08-12 16:43:15 +0900

    Feat: Send success status for landing page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
+// Send success status to Frontend
 const getLandingPage = (req, res) => {
-    return res.send("get home");
+    return res.sendStatus(200);
 };
 
 module.exports = { getLandingPage };
