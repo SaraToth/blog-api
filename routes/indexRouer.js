5aea54b31cd4fb2df0958fa7cb77a198a5d8f888
SaraToth <saratoth101@gmail.com>
2025-08-12 16:43:46 +0900

    Feat: Remove unnecessary middleware declaration

--- routes/indexRouer.js
+++ routes/indexRouer.js
@@ -1,7 +1,6 @@
 const { Router } = require("express");
 const indexRouter = Router();
 const { getLandingPage } = require("../controllers/indexController");
-const verifyToken = require("../middleware/verifyToken");
 
 indexRouter.get("/", getLandingPage);
 
