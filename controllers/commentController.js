ccb8e1c1eba257b047b6b7616e8caa72605dca6d
SaraToth <saratoth101@gmail.com>
2025-08-12 19:27:18 +0900

    Feat: Finalize edit comment logic

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -70,10 +70,47 @@ const editComment = [
         if (!errors.isEmpty()) {
             return res.sendStatus(400);
         }
+
         const userId = req.user?.id;
-        const { postTitle, commentId } = req.params;
+        const sluggedTitle = req.params.postTitle;
+        const commentId = parseInt(req.params.commentId);
         const { newContent } = req.body;
-        return res.send(`Edited comment ${commentId} on ${postTitle}`);
+
+        // Find the post
+        const post = await prisma.post.findFirst({
+            where: {
+                authorId: userId,
+                slug: sluggedTitle
+            }
+        });
+
+        if (!post) {
+            return res.status(404).send("File not found");
+        }
+
+        // Find the original comment
+        const originalComment = await prisma.comment.findUnique({
+            where: {
+                id: commentId
+            }
+        });
+
+        // If comment doesn't exist send error
+        if (!originalComment) {
+            return res.status(404).send("File not found");
+        }
+
+        // Update comment
+        await prisma.comment.update({
+            where: {
+                id: originalComment.id
+            },
+            data: {
+                content: newContent
+            }
+        });
+
+        return res.status(200).send("Edited comment");
     })
 ];
 
